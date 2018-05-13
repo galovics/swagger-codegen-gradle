5671c45c22befdb24cc6d53c791ed6a70c8de6ce
Arnold Galovics <galovicsarnold@gmail.com>
2018-05-13 16:55:54 +0200

    Customized swagger template to support return type with and without ResponseEntity

--- user-service/src/main/java/com/arnoldgalovics/blog/userservice/UserDelegateImpl.java
+++ user-service/src/main/java/com/arnoldgalovics/blog/userservice/UserDelegateImpl.java
@@ -3,19 +3,18 @@ package com.arnoldgalovics.blog.userservice;
 import com.arnoldgalovics.blog.userservice.api.UserApiDelegate;
 import com.arnoldgalovics.blog.userservice.model.ListUserResponse;
 import com.arnoldgalovics.blog.userservice.model.UserCreateRequest;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserDelegateImpl implements UserApiDelegate {
     @Override
-    public ResponseEntity<Void> createUser(UserCreateRequest user) {
+    public Void createUser(UserCreateRequest user) {
         // TODO: implementation
         return null;
     }
 
     @Override
-    public ResponseEntity<ListUserResponse> getUsers() {
+    public ListUserResponse getUsers() {
         // TODO: implementation
         return null;
     }
